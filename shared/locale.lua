8b83dc1e4acfc82b1f8fe71c3163b05f312a135c
DeaNox <50797437+DeaNox444@users.noreply.github.com>
2021-03-21 10:00:54 +0100

    Adding 3 langages in the locales.lua
    Hi! I added 3 languages in the locale.lua, so that it is usable for everyone! :D

--- shared/locale.lua
+++ shared/locale.lua
@@ -5,5 +5,26 @@ PZShared.registerAddonLocales({
         ["rank_updated"] = "Le rang du joueurs %s a été changé en %s",
         ["player_revived"] = "Joueur %s réanimé !",
         ["revived"] = "Vous avez été réanimé !"
+    },
+    ["EN"] = {
+        ["player_offline"] = "This player is disconnected!",
+        ["rank_missing"] = "This rank does not exist!",
+        ["rank_updated"] = "The rank of the player %s has been changed to %s",
+        ["player_revived"] = "Player %s revived!",
+        ["revived"] = "You have been revived!"
+    },
+    ["ES"] = {
+        ["player_offline"] = "¡Este jugador está desconectado!",
+        ["rank_missing"] = "¡Este rango no existe!",
+        ["rank_updated"] = "¡El rango del jugador %s ha sido cambiado a %s !",
+        ["player_revived"] = "¡Jugador %s revivido!",
+        ["revived"] = "¡Has sido revivido!"
+    },
+    ["DE"] = {
+        ["player_offline"] = "Dieser Player ist nicht angeschlossen!",
+        ["rank_missing"] = "Dieser Rang existiert nicht!",
+        ["rank_updated"] = "Der Spielerrang %s wurde auf %s geändert!",
+        ["player_revived"] = "Spieler %s wiederbelebt!",
+        ["revived"] = "Sie sind wiederbelebt worden!"
     }
 })
